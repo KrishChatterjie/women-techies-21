9354be0c206e88c924d9aef26c7692d054e856e3
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-02-07 22:49:54 +0530

    Nav changed tags

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -35,14 +35,14 @@ const Nav = ({ active, introRef, aboutRef, hackRef, visionRef, sponsorsRef }) =>
             <a href="https://dscvit.com/" target="_blank" rel="noreferrer"><img className="dsc-logo" src={dscLogo} alt="DSC Logo" /></a>
             <nav>
                 <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-                    <li><a className={`nav-home ${(active === 'intro') ? "nav-home-active" : ""}`} onClick={introScroll}>Home</a></li>
-                    <li><a className={`nav-about ${(active === 'about') ? "nav-about-active" : ""}`} onClick={aboutScroll}>About DSC</a></li>
-                    <li><a className={`nav-hack ${(active === 'hack') ? "nav-hack-active" : ""}`} onClick={hackScroll}>Hackathon</a></li>
-                    <li><a className={`nav-vision ${(active === 'vision') ? "nav-vision-active" : ""}`} onClick={visionScroll}>Vision</a></li>
+                    <li><span className={`nav-home ${(active === 'intro') ? "nav-home-active" : ""}`} onClick={introScroll}>Home</span></li>
+                    <li><span className={`nav-about ${(active === 'about') ? "nav-about-active" : ""}`} onClick={aboutScroll}>About DSC</span></li>
+                    <li><span className={`nav-hack ${(active === 'hack') ? "nav-hack-active" : ""}`} onClick={hackScroll}>Hackathon</span></li>
+                    <li><span className={`nav-vision ${(active === 'vision') ? "nav-vision-active" : ""}`} onClick={visionScroll}>Vision</span></li>
                     {/* <li><NavLink to="teaser" exact activeClassName='nav-active' onClick={() => setShow(true)}>Teaser</NavLink></li> */}
                     {/* <li><NavLink to="timeline" exact activeClassName='nav-active' onClick={() => setShow(true)}>Timeline</NavLink></li> */}
                     {/* <li><NavLink to="speakers" exact activeClassName='nav-active' onClick={() => setShow(true)}>Speakers</NavLink></li> */}
-                    <li><a className={`nav-sponsors ${(active === 'sponsors') ? "nav-sponsors-active" : ""}`} onClick={sponsorsScroll}>Sponsors</a></li>
+                    <li><span href="javascript:;" className={`nav-sponsors ${(active === 'sponsors') ? "nav-sponsors-active" : ""}`} onClick={sponsorsScroll}>Sponsors</span></li>
                     {/* <li><NavLink to="contact-us" exact activeClassName='nav-active' onClick={() => setShow(true)}>Contact Us</NavLink></li> */}
                     {/* <li><NavLink to="faq" exact activeClassName='active' onClick={() => setShow(true)}>FAQ's</NavLink></li> */}
                     <Modal show={show} onClose={() => setShow(false)} />
