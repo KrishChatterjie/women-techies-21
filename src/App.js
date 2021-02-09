07e1b48357984a3107843aa5595413fd061177a5
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-02-09 21:44:52 +0530

    Updated to clone current website

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { useState, useRef } from "react";
+import { useState, useRef } from 'react'
 import Nav from './components/Nav'
 import Home from './components/Home'
 
-function App() {
-  const [active, setActive] = useState('');
-  const introRef = useRef(null);
-  const aboutRef = useRef(null);
-  const hackRef = useRef(null);
-  const visionRef = useRef(null);
-  const sponsorsRef = useRef(null);
+function App () {
+  const [active, setActive] = useState('')
+  const introRef = useRef(null)
+  const aboutRef = useRef(null)
+  const hackRef = useRef(null)
+  const visionRef = useRef(null)
+  const sponsorsRef = useRef(null)
+  const contactRef = useRef(null)
 
   return (
     <>
-      <Nav active={active} introRef={introRef.current} aboutRef={aboutRef.current} hackRef={hackRef.current} visionRef={visionRef.current} sponsorsRef={sponsorsRef.current}/>
-      <Home setActive={setActive} introRef={introRef} aboutRef={aboutRef} hackRef={hackRef} visionRef={visionRef} sponsorsRef={sponsorsRef}/>
+      <Nav active={active} introRef={introRef.current} aboutRef={aboutRef.current} hackRef={hackRef.current} visionRef={visionRef.current} sponsorsRef={sponsorsRef.current} contactRef={contactRef.current} />
+      <Home setActive={setActive} introRef={introRef} aboutRef={aboutRef} hackRef={hackRef} visionRef={visionRef} sponsorsRef={sponsorsRef} contactRef={contactRef} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
